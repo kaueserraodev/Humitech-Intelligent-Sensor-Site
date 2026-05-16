9af4426ff172a9e2f1370bbf4541503491f6ef5a
kaueserraodev <kaueserraovatan01@gmail.com>
2026-05-16 12:34:16 -0300

    script.js

--- script.js
+++ script.js
@@ -1,56 +1,42 @@
 // CEP AUTOMÁTICO
+
 document.getElementById("cep").addEventListener("blur", function () {
+
   let cep = this.value.replace(/\D/g, "");
 
   if (cep.length !== 8) return;
 
   fetch(`https://viacep.com.br/ws/${cep}/json/`)
-    .then(res => res.json())
-    .then(dados => {
-      if (dados.erro) return;
 
-      document.getElementById("rua").value = dados.logradouro;
-      document.getElementById("bairro").value = dados.bairro;
-      document.getElementById("cidade").value = dados.localidade;
-      document.getElementById("estado").value = dados.uf;
+    .then(response => response.json())
+
+    .then(data => {
+
+      if (data.erro) {
+        alert("CEP não encontrado.");
+        return;
+      }
+
+      document.getElementById("rua").value = data.logradouro;
+      document.getElementById("bairro").value = data.bairro;
+      document.getElementById("cidade").value = data.localidade;
+      document.getElementById("estado").value = data.uf;
+
+    })
+
+    .catch(() => {
+      alert("Erro ao buscar CEP.");
     });
+
 });
 
-// FORM
-document.getElementById("form").addEventListener("submit", function(e) {
+
+// FORMULÁRIO
+
+document.getElementById("form").addEventListener("submit", function(e){
+
   e.preventDefault();
-  alert("Orçamento enviado 🚀");
-});
 
-// ANIMAÇÃO DE NÚMEROS
-function animarNumero(el, final) {
-  let atual = 0;
-  let incremento = final / 100;
+  alert("Orçamento enviado com sucesso 🚀");
 
-  let intervalo = setInterval(() => {
-    atual += incremento;
-    if (atual >= final) {
-      el.innerText = final + (final < 100 ? "%" : "");
-      clearInterval(intervalo);
-    } else {
-      el.innerText = Math.floor(atual);
-    }
-  }, 20);
-}
-
-// OBSERVER
-const observer = new IntersectionObserver(entries => {
-  entries.forEach(entry => {
-    if (entry.isIntersecting) {
-      let el = entry.target;
-      let valor = el.getAttribute("data-num");
-
-      animarNumero(el, valor);
-      observer.unobserve(el);
-    }
-  });
-});
-
-document.querySelectorAll(".numero").forEach(el => {
-  observer.observe(el);
 });
